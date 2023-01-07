80e09bd60b2d3d2b3cfe71e94ff66330cd9b8067
Wrzysztof <krzysztof13iv@gmail.com>
2023-01-07 16:20:29 +0100

    server attempt

--- src/main/java/com/example/checkers/client/ClientApplication.java
+++ src/main/java/com/example/checkers/client/ClientApplication.java
@@ -5,18 +5,50 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) {
 
         try {
 
-            ClientPane root = new ClientPane(stage);
-            Scene scene = new Scene(root);
+            //ClientPane root = new ClientPane(stage);
+            //Scene scene = new Scene(root);
 
-            stage.setTitle("Warcaby");
-            stage.setScene(scene);
-            stage.show();
+            //stage.setTitle("Warcaby");
+            //stage.setScene(scene);
+            //stage.show();
+
+            try (Socket socket = new Socket("localhost", 1234)) {
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                Scanner sc = new Scanner(System.in);
+                String line = null;
+
+                while(!"exit".equalsIgnoreCase(line)) {
+
+                    line = sc.nextLine();
+
+                    out.println(line);
+                    out.flush();
+
+                    //do game.something
+                }
+
+                sc.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         } catch(Exception e) {
 
--- src/main/java/com/example/checkers/server/ServerApplication.java
+++ src/main/java/com/example/checkers/server/ServerApplication.java
@@ -5,22 +5,103 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApplication extends Application {
     @Override
     public void start(Stage stage) {
 
         try {
 
-            ServerPane root = new ServerPane(stage);
-            Scene scene = new Scene(root);
+            ServerSocket server = null;
 
-            stage.setTitle("Warcaby - Server");
-            stage.setScene(scene);
-            stage.show();
+            try {
+
+                server = new ServerSocket(1234);
+                server.setReuseAddress(true);
+
+                while(true) {
+
+                    Socket client = server.accept();
+
+                    System.out.println("new clinet" + client.getInetAddress().getHostAddress());
+
+                    ClientHandler clientSock = new ClientHandler(client);
+
+                    new Thread(clientSock).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (server != null) {
+                    try {
+                        server.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
         } catch(Exception e) {
 
             e.printStackTrace();
+        }
+
+        ServerPane root = new ServerPane(stage);
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Warcaby - Server");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static class ClientHandler implements Runnable {
+
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+
+            PrintWriter out = null;
+            BufferedReader in = null;
+
+            try {
+
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String line;
+
+                while ((line = in.readLine()) != null) {
+
+                    System.out.printf("sent fdsf: %s\n", line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
