f6e92012a03ce40957032d5ee5edf84b42b68f0c
Wrzysztof <krzysztof13iv@gmail.com>
2023-01-17 21:01:58 +0100

    pawns disappearing bug fix

--- src/main/java/com/example/checkers/client/controller/pawns/MovingPawns.java
+++ src/main/java/com/example/checkers/client/controller/pawns/MovingPawns.java
@@ -1,6 +1,7 @@
 package com.example.checkers.client.controller.pawns;
 
 import com.example.checkers.client.view.boards.elements.Pawn;
+import javafx.application.Platform;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -11,7 +12,10 @@ public final class MovingPawns {
 
     public static void doMove(Pawn pawn, int x, int y) {
 
-        GridPane.setColumnIndex(pawn, GridPane.getColumnIndex(pawn.getBoard().getTile(x, y)));
-        GridPane.setRowIndex(pawn, GridPane.getRowIndex(pawn.getBoard().getTile(x, y)));
+        Platform.runLater(() -> {
+            GridPane.setColumnIndex(pawn, GridPane.getColumnIndex(pawn.getBoard().getTile(x, y)));
+            GridPane.setRowIndex(pawn, GridPane.getRowIndex(pawn.getBoard().getTile(x, y)));
+            pawn.toFront();
+        });
     }
 }
